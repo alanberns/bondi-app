57b5292765e7185515c3eb31f6500f23b83951cc
Alan Berns <52006716+alanberns@users.noreply.github.com>
2025-11-17 23:25:28 -0300

    Update HorariosArribos.jsx

--- src/pages/arribos/HorariosArribos.jsx
+++ src/pages/arribos/HorariosArribos.jsx
@@ -36,7 +36,7 @@ export default function HorariosArribos() {
     };
 
   return (
-    <div className="min-h-screen bg-[#F5F5F5] flex flex-col items-center justify-center px-4">
+    <div className="min-h-screen bg-[#F5F5F5] flex flex-col items-center justify-center p-4">
       <h1 className="text-3xl font-bold text-gray-800 mb-8 text-center">
         Próximos arribos
       </h1>
